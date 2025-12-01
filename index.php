5e1164877fa45b4e27a253c2570d40a199ae34e8
CM-Kymiira <Kymiirastuff@gmail.com>
2025-12-01 09:42:51 -0800

    Change magic word from 'fred' to 'caleb'

--- index.php
+++ index.php
@@ -1,30 +1,20 @@
 <?php
-// Initialize variables for the result message and the input text.
-$checkResult = "<span style='color:red'> Try the magic word 'fred'</span>";
+$checkResult = "<span style='color:red'> Try the magic word 'caleb'</span>";
 $myInputText01 = '';
 
-// Check if the form was submitted (i.e., if the request method is POST)
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Get the input text from the form data.
-    // We use isset() and the null coalescing operator (??) for safety.
     $myInputText01 = $_POST['myText01'] ?? '';
 
-    // The logic from the Node.js application
     $myCheck = false;
-    if ($myInputText01 === 'fred') {
+    if ($myInputText01 === 'caleb') {
         $myCheck = true;
     }
-
-    // Determine the result message and color
     if ($myCheck) {
         $checkResult = "<b style='color:green'> Cool! </b>";
     } else {
-        $checkResult = "<span style='color:red'> Try the magic word 'fred'</span>";
+        $checkResult = "<span style='color:red'> Try the magic word 'caleb'</span>";
     }
 }
-
-// Now, render the HTML page, embedding the dynamic content.
-// The form's 'action' is set to the current file (index.php) and the method is 'post'.
 ?>
 <!DOCTYPE html>
 <html>
@@ -37,9 +27,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     <form action="" method="post" >
         <label for="myText01">Enter Text:</label>
-        <!-- The 'value' attribute keeps the entered text after submission -->
    <!-- <input type="text" id="myText01" name="myText01" value="<?= htmlspecialchars($myInputText01); ?>"> -->
-        <input type="text" id="myText01" name="myText01" value="">
+        <input type="text" id="myText01" name="myText01" value="caleb">
         <input type="submit" value="Submit">
     </form>
     
