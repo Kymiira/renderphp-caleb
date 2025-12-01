d80c470010c4ebb2752fbc3b4da719733e8e23ee
CM-Kymiira <Kymiirastuff@gmail.com>
2025-12-01 09:52:40 -0800

    Change page title to 'phpisbad'

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Browser Title</title>
+    <title>phpisbad</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
